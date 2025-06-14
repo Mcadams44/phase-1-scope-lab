46b696a7fa02ca0b3c5e38de547183160bd629fb
Mcadams <mcadamsproject@gmail.com>
2025-06-14 14:53:44 +0300

    kawine

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+// 1. Declare customerName in global scope
+var customerName = 'bob';
+
+// 2. Modify customerName to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Attempt to reassign constant (should throw error)
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'initial value';
+  leastFavoriteCustomer = 'new value'; // This will throw an error when called
+}
